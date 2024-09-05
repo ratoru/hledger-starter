a97bcb63bb6e0708c7a814168b6376933c127b8b
Raphael <raphael.t.ruban@gmail.com>
2024-09-05 17:06:33 +0200

    feat: quit when no unknown transactions left

--- resolve.sh
+++ resolve.sh
@@ -3,22 +3,24 @@ set -e -o pipefail
 
 cat ./export/*-accounts.txt | sort -u >/tmp/accounts.txt
 
-while true ; do
+while true; do
     # Choose one of the unknown transactions' description
-    transactions=$(cat ./export/*unknown.journal \
-        | hledger -f - register -I -O csv \
-        | grep -v ":unknown" \
-        | grep -v -f <(cat ./import/*/rules.psv | cut -d'|' -f1) \
-        | mlr --csv cut -f date,description,account,amount
+    transactions=$(
+        cat ./export/*unknown.journal |
+            hledger -f - register -I -O csv |
+            grep -v ":unknown" |
+            grep -v -f <(cat ./import/*/rules.psv | cut -d'|' -f1) |
+            mlr --csv cut -f date,description,account,amount
     )
     count=$(echo "${transactions}" | mlr count)
-    if [ "$count" == "count=1" ] ; then
+    if [ "$count" == "count=1" ]; then
         echo "No unknown transactions left!"
         exit 0
     fi
-    description_account=$(echo "${transactions}" \
-        | fzf --header="Choose transaction" --wrap --border --border-label "Choose Transaction" --tac -d ',' --header-lines=1 --nth 2 \
-        | mlr --csv --implicit-csv-header --headerless-csv-output cut -f 2,3
+    description_account=$(
+        echo "${transactions}" |
+            fzf --header="Choose transaction" --wrap --border --border-label "Choose Transaction" --tac -d ',' --header-lines=1 --nth 2 |
+            mlr --csv --implicit-csv-header --headerless-csv-output cut -f 2,3
     )
 
     # Description_account is "<description>,<source account that money came from>"
@@ -51,32 +53,35 @@ while true ; do
             --print-query \
             --bind "start:$RELOAD" --bind "change:$RELOAD" \
             --bind 'ctrl-d:delete-char' \
-            --query "${description}" \
-            | head -n1
+            --query "${description}" |
+            head -n1
     }
     regexp=$(tuning ${description} ${dir})
 
     # Now lets choose account
-    account=$(cat /tmp/accounts.txt \
-        | sort -u \
-        | fzf --header="Choose account" --wrap \
-            --border --border-label "Choose Account"
+    target_account=$(
+        cat /tmp/accounts.txt |
+            sort -u |
+            fzf --header="Choose account money was sent to (prepend : to create new account)" --wrap \
+                --border --border-label "Choose Account" \
+                --bind "enter:accept-or-print-query" |
+            sed -e 's/^://'
     )
 
-    cat ${dir}/rules.psv | cut -d '|' -f3 | sort -u > /tmp/comments.txt
+    cat ${dir}/rules.psv | cut -d '|' -f3 | sort -u >/tmp/comments.txt
 
     # ... and comment. Comment could be either selected from existing comments
     # or just entered. When entered comment is a substring match of one of the existing comments,
     # you can prepend your comment with ":" to prevent the match from being selected
     comment=$(
         RELOAD="reload:rg --color=always --line-number {q} /tmp/comments.txt || :"
-        fzf --header="Enter comment (leave blank for no comment)" --disabled --ansi \
+        fzf --header="Enter comment (prepend : to inhibit selection)" --disabled --ansi \
             --wrap --border --border-label "Enter optional comment" \
             --bind "start:$RELOAD" --bind "change:$RELOAD" \
-            --bind "enter:accept-or-print-query" \
-        | cut -d ':' -f2
+            --bind "enter:accept-or-print-query" |
+            cut -d ':' -f2
     )
 
-    echo "${regexp}|${account}|${comment}" >> "${dir}/rules.psv"
-    echo "Added rule: ${regexp}|${account}|${comment} to ${dir}/rules.psv"
+    echo "${regexp}|${target_account}|${comment}" >>"${dir}/rules.psv"
+    echo "Added rule: ${regexp}|${target_account}|${comment} to ${dir}/rules.psv"
 done
